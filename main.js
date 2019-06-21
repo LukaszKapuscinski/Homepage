1f011b0863227245d180b8ebba840627f41b9d5f
Lukasz Kapuscinski <lukasz.dawid.kapuscinski@gmail.com>
2019-06-21 22:44:15 +0100

    class name change 2

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('Hell O!');
+const name = 'Lukasz Kapuscinski';
+console.log(name);
+
+
+console.log(`Nazywam się ${name} `);
+
+console.log('witaj przybyszu 🚀');
+
+
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about);
+
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
